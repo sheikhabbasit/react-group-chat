4a883808ce66fbda1a0fec9e9b629329bd20b3f5
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2024-07-18 21:45:54 +0530

    feat(): adds background colors and styling

--- src/styles/useAppStyles.js
+++ src/styles/useAppStyles.js
@@ -1,10 +1,12 @@
 export const useAppStyles = () => {
   const styles = {
     appContainer: {
-      backgroundColor: "#FFFFFF",
+      backgroundColor: "rgba(17, 25, 40, 0.75)",
+      backdropFilter: "blur(20px) saturate(280%)",
       height: "90vh",
       width: "90vw",
       margin: "5vh auto auto",
+      borderRadius: "12px",
     },
   };
   return styles;
