3c2b75c6f8718209d2013c8fc4f11a9032af6aa1
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2024-07-18 21:23:43 +0530

    feat(): main container styling

---
+++ src/styles/useAppStyles.js
@@ -0,0 +1,11 @@
+export const useAppStyles = () => {
+  const styles = {
+    appContainer: {
+      backgroundColor: "#FFFFFF",
+      height: "90vh",
+      width: "90vw",
+      margin: "5vh auto auto",
+    },
+  };
+  return styles;
+};
