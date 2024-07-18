a5b6d588365bb149a266a059987c874a951b6d1d
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2024-07-18 21:22:57 +0530

    feat(): MUI theming setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider, Typography } from "@mui/material";
+import { defaultTheme } from "./themes/defaultTheme";
+import { useCommonStyles } from "./styles/useCommonStyles";
+import { useAppStyles } from "./styles/useAppStyles";
 
 const App = () => {
-  return <Box className="">App</Box>;
+  const currentTheme = createTheme(defaultTheme);
+  const commonStyles = useCommonStyles();
+  const appStyles = useAppStyles();
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <Box sx={[commonStyles.fullyCentered, appStyles.appContainer]}>
+        <Typography variant="h1">React Firebase Chat</Typography>
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default App;
