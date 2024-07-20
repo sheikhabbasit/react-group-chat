196444a58edb37e735249f5d7280cadb869c7d3e
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2024-07-20 12:04:19 +0530

    feat(): login and signup UIs

---
+++ src/components/Login/Login.controller.js
@@ -0,0 +1,3 @@
+export const useLoginController = () => {
+  return {};
+};
--- src/pages/MainApp/MainApp.jsx
+++ src/pages/MainApp/MainApp.jsx
@@ -3,16 +3,24 @@ import Chat from "../../components/Chat/Chat";
 import List from "../../components/List/List";
 import { useAppStyles } from "../../styles/useAppStyles";
 import { useCommonStyles } from "../../styles/useCommonStyles";
+import Login from "../../components/Login/Login";
 
 function MainApp() {
   const appStyles = useAppStyles();
   const commonStyles = useCommonStyles();
+  const user = false;
 
   return (
     <Box sx={commonStyles.fullyCentered}>
       <Box sx={appStyles.appContainer}>
-        <List />
-        <Chat />
+        {user ? (
+          <>
+            <List />
+            <Chat />
+          </>
+        ) : (
+          <Login />
+        )}
       </Box>
     </Box>
   );
